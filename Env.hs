4d714a685470fbef9a279dcc63525366d8af1008
reinvdwoerd <reinvanderwoerd@me.com>
2017-03-14 14:25:23 +0100

    env

--- Env.hs
+++ Env.hs
@@ -9,17 +9,55 @@ import           Error
 import           Val
 
 
-type Env = IORef [(String, LispVal)]
+type Env = IORef [(String, IORef LispVal)]
 
 
 newEnv :: IO Env
 newEnv = newIORef []
 
+isBound :: Env -> String -> IO Bool
+isBound envRef var = do
+  env <- readIORef envRef
+  return $ isJust $ lookup var env
+
+
+setVar :: Env -> String -> LispVal -> IOThrowsError LispVal
+setVar envRef var value = do
+  env <- liftIO $ readIORef envRef
+  maybe (throwError $ UnboundVar var)
+        (liftIO . (flip writeIORef value))
+        (lookup var env)
+  return value
+
+
 getVar :: Env -> String -> IOThrowsError LispVal
 getVar envRef var = do
   env <- liftIO $ readIORef envRef
-  case lookup var env of
-    Nothing ->
-      throwError $ UnboundVar var
-    Just val ->
-      return val
+  maybe (throwError $ UnboundVar var)
+        (liftIO . readIORef)
+        (lookup var env)
+
+defineVar :: Env -> String -> LispVal -> IOThrowsError LispVal
+defineVar envRef var value = do
+     alreadyDefined <- liftIO $ isBound envRef var
+     if alreadyDefined
+        then setVar envRef var value >> return value
+        else liftIO $ do
+             valueRef <- newIORef value
+             env <- readIORef envRef
+             writeIORef envRef ((var, valueRef) : env)
+             return value
+
+
+bindVars :: Env -> [(String, LispVal)] -> IO Env
+bindVars envRef bindings = readIORef envRef >>= extendEnv bindings >>= newIORef
+     where extendEnv bindings env = liftM (++ env) (mapM addBinding bindings)
+           addBinding (var, value) = do ref <- newIORef value
+                                        return (var, ref)
+
+-- getBindings :: Env -> IOThrowsError Bindings
+-- getBindings envRef = do
+--   envRecord <- liftIO $ readIORef envRef
+--   return $ bindings envRecord
+
+-- lookupVar :: Env -> IOThrowsError LispVal
