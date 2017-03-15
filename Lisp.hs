177d968acfd788c42350b402972432f88dcad13f
reinvdwoerd <reinvanderwoerd@me.com>
2017-03-15 08:59:10 +0100

    print errors red

--- Lisp.hs
+++ Lisp.hs
@@ -4,6 +4,7 @@ import           Control.Monad.Trans.Except
 import           Env
 import           Eval
 import           Parse
+import           System.Console.ANSI
 import           Types
 
 
@@ -16,9 +17,14 @@ repl env = do
   putStr "lisp=> "
   >>  getLine
   >>= evalString env
-  >>= either print print
+  >>= either printError print
   >>  repl env
 
 evalString :: Env -> String -> IO (Either LispError LispVal)
 evalString env expr =
   runExceptT (parseLine expr >>= eval env)
+
+printError err = do
+  setSGR [SetColor Foreground Vivid Red]
+  print err
+  setSGR [Reset]
