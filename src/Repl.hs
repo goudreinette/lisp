0a3d9a648e19f08bf98e20df82945b8aca95caf8
reinvdwoerd <reinvanderwoerd@me.com>
2017-04-20 15:28:56 +0200

    Use new repl package

--- src/Repl.hs
+++ src/Repl.hs
@@ -1,35 +1,26 @@
 module Repl where
 
-import           Control.Monad.Trans
 import           Control.Monad.Trans.Except
 import           Env
 import           Eval
 import           Parse
 import           System.Console.ANSI
-import           System.Console.Haskeline
-import           System.IO
+import qualified System.Console.Repl        as R
 import           Types
 
 
 
 repl = do
   globalEnv <- newEnv
-  runInputT defaultSettings $ loop globalEnv
-
-loop env = do
-  line <- getInputLine "lisp=> "
-  case line of
-    Nothing -> return ()
-    Just expr -> do
-      evaled <- liftIO $ evalString env expr
-      liftIO $ either printError print evaled
-      loop env
+  R.repl "lisp=> " $ evalString globalEnv
 
 
 
-evalString :: Env -> String -> IO (Either LispError LispVal)
-evalString env expr =
-  runExceptT (parseLine expr >>= eval env)
+evalString :: Env -> String -> IO ()
+evalString env expr = do
+  result <- runExceptT (parseLine expr >>= eval env)
+  either printError print result
+
 
 printError err = do
   setSGR [SetColor Foreground Vivid Red]
