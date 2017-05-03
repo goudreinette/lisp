972619b2d4b5b936923ab730e8fb6c79ce2de75e
reinvdwoerd <reinvanderwoerd@me.com>
2017-05-03 16:01:10 +0200

    optparse

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,30 @@
 module Main where
 
+import           Control.Monad       (join, when)
+import           Data.Monoid         ((<>))
 import           Env
 import           Eval
+import           Options.Applicative
 import           Primitives
 import           System.Console.Repl
-import           System.Environment  (getArgs)
 
 
 
 
-main = do
-  args <- getArgs
+main = join . customExecParser (prefs showHelpOnError) $
+  info (helper <*> parser)
+  (  fullDesc
+  <> header "Lisp"
+  )
+  where parser :: Parser (IO ())
+        parser = work
+            <$> optional (argument str (metavar "FILE"))
+            <*> switch (long "interactive" <> short 'i' <> help "Launch a repl session")
+
+work :: Maybe FilePath -> Bool -> IO ()
+work file i = do
   globalEnv <- newEnv primitives
-  case args of
-    [file] ->
-      evalFile globalEnv file
-    [file, "-i"] -> do
-      evalFile globalEnv file
-      interactive globalEnv
-    [] ->
-      interactive globalEnv
+  maybe (return ()) (evalFile globalEnv) file
+  when i $ interactive globalEnv
   where interactive env =
           repl "lisp=> " $ evalString env
